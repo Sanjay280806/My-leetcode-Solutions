ed7d8303d06ce6e4c073e102531a846c51b17d7a
Sanjay <sanjay2808a@gmail.com>
2026-01-14 20:21:10 +0530

    Intersection of two array

---
+++ ARRAYS/Intersection.cpp
@@ -0,0 +1,46 @@
+/*
+    Problem: Intersection of Two Arrays
+    LeetCode: https://leetcode.com/problems/intersection-of-two-arrays/
+    Difficulty: Easy
+    
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+
+        for (auto x : nums1) {
+            for (auto y : nums2) {
+                if (x == y) {
+                    if (find(result.begin(), result.end(), x) == result.end()) {
+                        result.push_back(x);
+                    }
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+
+    vector<int> ans = sol.intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int x : ans) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
