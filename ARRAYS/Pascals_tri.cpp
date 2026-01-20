edb797d0fe9827691c92b6ae7f6ab37fcdf117ea
Sanjay <sanjay2808a@gmail.com>
2026-01-20 21:27:32 +0530

    Pascal's Triangle

---
+++ ARRAYS/Pascals_tri.cpp
@@ -0,0 +1,47 @@
+/*
+LeetCode No.118 - Pascal's Triangle
+https://leetcode.com/problems/pascals-triangle/
+
+--------------------------------------------------------
+TIME COMPLEXITY: O(n^2)
+SPACE COMPLEXITY: O(n^2)
+--------------------------------------------------------
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1);
+
+            for (int j = 1; j < i; j++) {
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+
+            triangle.push_back(row);
+        }
+
+        return triangle;
+    }
+};
+
+int main() {
+    int numRows;
+    cin >> numRows;
+
+    Solution obj;
+    vector<vector<int>> result = obj.generate(numRows);
+
+    for (auto row : result) {
+        for (int x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
