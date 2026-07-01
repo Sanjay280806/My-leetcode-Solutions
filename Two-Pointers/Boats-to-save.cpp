e9f84d8ea6f8835dac20d2641e83bf6b32429a1a
Sanjay <sanjay2808a@gmail.com>
2026-07-01 12:03:18 +0530

    Boats to Save People

---
+++ Two-Pointers/Boats-to-save.cpp
@@ -0,0 +1,26 @@
+//881. Boats to Save People
+// You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+// Return the minimum number of boats to carry every given person.
+//https://leetcode.com/problems/boats-to-save-people/submissions/2051929899/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int left = 0;
+        int right = people.size() - 1;
+        int boats =0;
+
+        while(left<=right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+        
+    }
+};
